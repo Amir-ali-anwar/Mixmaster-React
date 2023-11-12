fc3961b5cdae6dc3007a613d1aaf7896be8e81d8
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-12 13:48:59 +0500

    setting up the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter(
+  [
+    {
+      path:'/',
+      element:<h2>homepage</h2>
+    }
+  ]
+)
 const App = () => {
   return <h2>mixmaster starter</h2>;
 };
