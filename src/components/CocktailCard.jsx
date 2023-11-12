e0482280821ba762badcfb0fdec2810571cedd18
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-12 22:55:35 +0500

    added link for cocktail detail

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CocktailsCard from '../assets/wrappers/CocktailCard'
+import { Link } from 'react-router-dom';
 const CocktailCard = ({id,name,image,info,glass}) => {
   console.log(glass);
   return (
@@ -11,6 +12,7 @@ const CocktailCard = ({id,name,image,info,glass}) => {
       <h4>{name}</h4>
       <h5>{glass}</h5>
       <p>{info}</p>
+      <Link to={`/cocktails/${id}`}>View details</Link>
       </div>
     </CocktailsCard>
   )
