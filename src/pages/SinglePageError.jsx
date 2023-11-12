3865e1afd8fac456375e2f28b35a4a319289a933
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-12 22:01:53 +0500

    setting up the single page error

---
+++ src/pages/SinglePageError.jsx
@@ -0,0 +1,9 @@
+import { useLoaderData } from "react-router-dom"
+
+const SinglePageError = () => {
+   const error= useLoaderData()
+    // return <h2>{error.message}</h2>;
+  return <h2>{`${`there was an error... ${error}`}`}</h2>;
+}
+
+export default SinglePageError
