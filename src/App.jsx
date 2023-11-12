b884c950aec4afedcaffd1e3bfde8286796d7b0a
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-12 14:03:47 +0500

    setting up the page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {Error} from './pages'
 const router = createBrowserRouter(
   [
     {
       path:'/',
-      element:<h2>homepage</h2>
+      element:<h2>homepage</h2>,
+      errorElement:<Error />,
+      children:[
+        
+        {
+          path:'/about',
+          element:<h2>aboutpage</h2>
+        }
+      ]
     }
   ]
 )
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import About from './About'
+import Cocktail from './Cocktail'
+import Error from './Error'
+import HomeLayout from './HomeLayout.jsx'
+import Landing from './Landing.jsx'
+import NewsLetter from './NewsLetter.jsx'
+export  {
+    About,
+    Cocktail,
+    Error,
+    HomeLayout,
+    Landing,
+    NewsLetter
+}
