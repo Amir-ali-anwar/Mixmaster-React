3caed50550d1f667249998bf8e8208de256a666d
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-13 22:52:24 +0500

    setting up the details page

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,61 +1,87 @@
-import axios from 'axios';
-import React from 'react'
-import { Link, useLoaderData } from 'react-router-dom';
-import CockTailWrapper from '../assets/wrappers/CocktailPage';
+import axios from "axios";
+import React from "react";
+import { Link, Navigate, useLoaderData } from "react-router-dom";
+import CockTailWrapper from "../assets/wrappers/CocktailPage";
 const singleCocktailUrl =
-  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+  "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
-export const loader= async({params})=>{
-const {id}= params;
-const {data}= await axios.get(`${singleCocktailUrl}${id}`)
-return {id,data}
-}  
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+  return { id, data };
+};
 const Cocktail = () => {
-  const {id,data}= useLoaderData()
+  const { id, data } = useLoaderData();
+  if(!data) return <Navigate to='/' />
   const singleDrink = data?.drinks[0];
+  //  converting the interigents into array method 1
+  // const validData = Object.keys(singleDrink).filter((item) => {
+  //   return item.startsWith("strIngredient");
+  // });
+  // const ingredientsArray= validData?.map((item)=>singleDrink[item]).filter((value)=> value !== null)
+  // console.log(ingredientsArray);
 
-const {
-  strDrink: name,
-  strDrinkThumb: image,
-  strAlcoholic: info,
-  strCategory: category,
-  strGlass: glass,
-  strInstructions: instructions,
-} = singleDrink;
+  // Method 2
+
+  const validIngeridents= Object.keys(singleDrink).filter((item)=>item.startsWith("strIngredient")).reduce((acc,curr)=>{
+      const value= singleDrink[curr]
+      if(value !== null){
+        acc.push(value)
+      }
+      return acc
+  },[])
+  console.log(validIngeridents);
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
 
   return (
     <CockTailWrapper>
       <header>
-        <Link to='/' className='btn'>
+        <Link to="/" className="btn">
           Back to Home
         </Link>
       </header>
-      <div className='drink'>
+      <div className="drink">
         <img src={image} alt="" />
-        <div className='drink-info'>
+        <div className="drink-info">
           <p>
-            <span className='drink-data'>name:</span>{name}
+            <span className="drink-data">name:</span>
+            {name}
           </p>
           <p>
-            <span className='drink-data'>category :</span>
+            <span className="drink-data">category :</span>
             {category}
           </p>
           <p>
-            <span className='drink-data'>info :</span>
+            <span className="drink-data">info :</span>
             {info}
           </p>
           <p>
-            <span className='drink-data'>glass :</span>
+            <span className="drink-data">glass :</span>
             {glass}
           </p>
           <p>
-            <span className='drink-data'>instructions :</span>
+            <span className="drink-data">instructions :</span>
             {instructions}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {
+              validIngeridents?.map((singleIng,index)=>{
+               return <span className="ing" key={index}>{singleIng},</span>
+              })
+            }
           </p>
         </div>
       </div>
     </CockTailWrapper>
-  )
-}
+  );
+};
 
-export default Cocktail
+export default Cocktail;
