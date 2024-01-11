218e61c75b8d5d44731b659287d029f775c754c6
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-01-11 12:21:27 +0500

    updated the React query defaultOptions

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 2,
+      refetchOnMount:false,
+      refetchOnWindowFocus:false,
+      retry:2
     },
   },
 });
